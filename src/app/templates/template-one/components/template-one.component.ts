f19707f7ab4b75bf07f00406466f160c7ea04c4c
Dwivedi,Abhinav <abhinav.dwivedi@jpmchase.com>
2018-05-16 18:30:27 +0530

    FIXING MAX AND MIN

--- src/app/templates/template-one/components/template-one.component.ts
+++ src/app/templates/template-one/components/template-one.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, AfterViewChecked, ElementRef  } from '@angular/core';
+import { trigger, style, transition, animate, state, keyframes, query, stagger } from '@angular/animations';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/scan';
 import { Message } from '../../models/message';
@@ -7,7 +8,51 @@ import { TemplateOneService } from '../services/template-one.service';
 @Component({
   selector: 'app-template-one',
   templateUrl: './template-one.component.html',
-  styleUrls: ['./template-one.component.css']
+  styleUrls: ['./template-one.component.css'],
+  animations: [
+    trigger('minOrMaxWindow', [
+      state('inactive', style({
+        borderRadius: '8px 8px 8px 8px',
+        boxShadow: '0px 0px 10px 2px',
+        width: '376px',
+        position: 'fixed',
+        float: 'right',
+        bottom: '70px',
+        right: '20px',
+        backgroundColor: '#FFFFFF'
+      })),
+      state('active',   style({
+        borderRadius: '8px 8px 8px 8px',
+        boxShadow: '0px 0px 10px 2px',
+        width: '376px',
+        position: 'fixed',
+        float: 'right',
+        bottom: '70px',
+        right: '20px',
+        backgroundColor: '#FFFFFF',
+        height: '500px'
+      })),
+      transition('inactive => active', animate('100ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out'))
+    ]),
+    trigger('minOrMaxBody', [
+      state('inactive', style({
+        marginLeft: '-15px',
+        width: '376px',
+        backgroundColor: '#363636',
+        borderRadius: '8px 8px 8px 8px'
+      })),
+      state('active',   style({
+        marginLeft: '-15px',
+        width: '376px',
+        backgroundColor: '#363636',
+        borderRadius: '8px 8px 8px 8px',
+        height: '500px'
+      })),
+      transition('inactive => active', animate('100ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out'))
+    ])
+  ]
 })
 export class TemplateOneComponent implements OnInit, AfterViewChecked {
 
@@ -16,6 +61,7 @@ export class TemplateOneComponent implements OnInit, AfterViewChecked {
   isPlusSign = true;
   isMinusSign = false;
   todayDate: Date = new Date();
+  minOrMax = 'inactive';
 
   @ViewChild('chatList', { read: ElementRef }) chatList: ElementRef;
 
@@ -50,10 +96,14 @@ export class TemplateOneComponent implements OnInit, AfterViewChecked {
   maximize() {
     this.isPlusSign = false;
     this.isMinusSign = true;
+    this.minOrMax = 'active';
+    console.log('active');
   }
 
   minimize() {
     this.isMinusSign = false;
     this.isPlusSign = true;
+    this.minOrMax = 'inactive';
+    console.log('inactive');
   }
 }
